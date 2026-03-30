4b6297c0969f77d7bd9dfc4ffbd7517ebf749ac8
Đức Thắng <ducthang@MacBook-Pro-cua-Thang.local>
2026-03-30 21:55:18 +0700

    change realtime1

--- frontend/sanity/lib/live.ts
+++ frontend/sanity/lib/live.ts
@@ -1,6 +1,5 @@
 import {defineLive} from 'next-sanity/live'
 import {client} from '@/sanity/lib/client'
-import {token} from '@/sanity/lib/token'
 
 /**
  * Use defineLive to enable automatic revalidation and refreshing of your fetched content
@@ -10,7 +9,7 @@ import {token} from '@/sanity/lib/token'
 export const {sanityFetch, SanityLive} = defineLive({
   client,
   // Required for showing draft content when the Sanity Presentation Tool is used, or to enable the Vercel Toolbar Edit Mode
-  serverToken: token,
+  serverToken: process.env.SANITY_API_READ_TOKEN,
   // Required for stand-alone live previews, the token is only shared to the browser if it's a valid Next.js Draft Mode session
-  browserToken: token,
+  browserToken: process.env.SANITY_API_READ_TOKEN,
 })
